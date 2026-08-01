7b96757e8b869c6de57e12987281df0ea30a6262
sugandhakc30-cmyk <sugandhakc30@gmail.com>
2026-08-01 23:34:25 +0545

    Add sitemap support

--- sugandha-tech/vite.config.ts
+++ sugandha-tech/vite.config.ts
@@ -2,10 +2,16 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import Sitemap from 
+'vite-plugin-sitemap';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      Sitemap()],
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
