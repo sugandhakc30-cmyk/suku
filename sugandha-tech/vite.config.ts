a473ccdf8f8de00bec8640d7cdfabe1c4a99860e
sugandhakc30-cmyk <sugandhakc30@gmail.com>
2026-08-04 16:22:40 +0545

    Update vite.config.ts

--- sugandha-tech/vite.config.ts
+++ sugandha-tech/vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { AMAZON_PRODUCTS } from './data/amazonProducts';
+import { AMAZON_PRODUCTS } from './data/amazonProducts.ts';
 import path from 'path';
 import {defineConfig} from 'vite';
 import Sitemap from 
