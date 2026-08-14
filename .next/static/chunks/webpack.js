bc0f4394a8ce81e8fb1cdcf0c574a5d11d3eacd1
sugandhakc30-cmyk <sugandhakc30@gmail.com>
2026-08-15 00:17:56 +0545

    app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f5a4bf23c7788c78"; }
+/******/ 		__webpack_require__.h = function() { return "882ddbc93e49c514"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
