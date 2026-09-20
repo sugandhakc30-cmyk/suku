2e211dc48fd8b24f083593be83912e7c4268bdf1
sugandhakc30-cmyk <sugandhakc30@gmail.com>
2026-09-21 00:42:23 +0545

    app-build-manifest.json

---
+++ .next/static/chunks/app/privacy-policy/page-ee216717cdbba968.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[310,301,808,800,970,327,873,571],{3521:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- cookie-policy/page.tsx
+++ cookie-policy/page.tsx
@@ -1,3 +1,4 @@
+
 export default function CookiePolicy() {
   return (
     <div>
